af2e9eeec0efae40b7fdf4f88f1312758c7c18e5
ysz <1582945670@qq.com>
2019-08-11 23:49:22 +0800

    categories UI

---
+++ admin/src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Main from './views/Main.vue'
+import CategoryEdit from './views/CategoryEdit.vue'
+import CategoryList from './views/CategoryList.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: Main,
+      children: [
+        { path: '/categories/create', component: CategoryEdit },
+        { path: '/categories/edit/:id', component: CategoryEdit, props: true },
+        { path: '/categories/list', component: CategoryList },
+      ]
+    },  
+  ]
+})
